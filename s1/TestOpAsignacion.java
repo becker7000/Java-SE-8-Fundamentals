439692a29ab37569e9b60ad3dd1e80c1c902c534
Erick Alvarez <becker7000@outlook.es>
2022-06-13 12:53:43 -0500

    ternario

---
+++ s1/TestOpAsignacion.java
@@ -0,0 +1,17 @@
+public class TestOpAsignacion {
+
+    public static void main(String[] args) {
+
+        int ahorro=0;
+
+        ahorro+=200; //Lunes
+        ahorro*=2;
+        ahorro+=140;
+        ahorro-=100;
+        ahorro*=2;
+
+        System.out.printf("\n\t Ahorro total: %d",ahorro);
+
+    }
+
+}
