4ad705a872255615af7e41d908a4c19e79fcb116
Erick Alvarez <becker7000@outlook.es>
2022-06-15 16:28:52 -0500

    Clases swing

---
+++ s3/TestVentana.java
@@ -0,0 +1,12 @@
+public class TestVentana {
+
+    public static void main(String[] args) {
+
+        Ventana ventana = new Ventana("Aquí va un título increíble",500,500);
+
+        //Por defecto todas las ventanas en Java son invisibles:
+        ventana.setVisible(true);
+
+    }
+
+}
