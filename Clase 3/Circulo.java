1b4593ac2edf4b9e7af03facb663d99b85d9cb75
Erick Alvarez <becker7000@outlook.es>
2022-06-29 12:28:19 -0500

    Circulo modificado

--- Clase 3/Circulo.java
+++ Clase 3/Circulo.java
@@ -1,11 +1,23 @@
 public class Circulo {
 
+    //ATRIBUTOS:
     private double radio;
+    // Sintaxis para una variable estática:
+    private static int cantidadDeCirculos=0;
 
+    //CONSTRUCTORES:
     public Circulo(double radio) {
         this.radio = radio;
+        cantidadDeCirculos++; // Incrementamos su valor al crear cada circulo
     }
 
+    public Circulo(){ //Sobrecargando el constructor
+        this.radio=1; // Circulo unitario...
+        cantidadDeCirculos++;
+    }
+
+
+    // GETTERS Y SETTERS
     public double getRadio() {
         return radio;
     }
@@ -14,6 +26,7 @@ public class Circulo {
         this.radio = radio;
     }
 
+    // MÉTODOS PERSONALIZADOS:
     public double calcularArea(){
         double area;
         area=Math.PI*Math.pow(radio,2);
@@ -27,6 +40,19 @@ public class Circulo {
         return sumaAreas;
     }
 
+    // Sobrecargando el método sumarAreaCon
+    public double sumarAreaCon(double area){
+        double sumaAreas;
+        sumaAreas = this.calcularArea()+area;
+        return sumaAreas;
+    }
+
+    //MÉTODOS ESTÁTICOS:
+    public static void cuantosCirculosHay(){
+        System.out.printf("\n\t Al momento van %d circulos creados...",cantidadDeCirculos);
+    }
+
+    //TOSTRING
     @Override
     public String toString() {
         return "Circulo{" +
--- Clase 3/TestCirculo.java
+++ Clase 3/TestCirculo.java
@@ -8,9 +8,18 @@ public class TestCirculo {
         Circulo circulo2 = new Circulo(10.2);
         System.out.println(circulo2);
 
+        Circulo circulo3 = new Circulo(2);
+        Circulo circulo4 = new Circulo(); //Le asigna en automatico radio=1
+
         System.out.println("\n\t Area C1: "+circulo1.calcularArea());
         System.out.println("\n\t Area C2: "+circulo2.calcularArea());
         System.out.println("\n\t Suma de las areas: "+circulo1.sumarAreaCon(circulo2));
+
+        System.out.println("\n\t Suma A3 + A4 = "+
+                circulo3.sumarAreaCon(circulo4.calcularArea()));
+        // Forma de usar un método estático
+        // No requerimos instanciar ningún objeto...
+        Circulo.cuantosCirculosHay();
 
     }
 
